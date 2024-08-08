1d3132c7b998c33cb950c13e3d4b9ac603b70e3b
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-08 23:53:31 +0530

    added comments

--- routes/todos.js
+++ routes/todos.js
@@ -10,6 +10,7 @@
 const express = require("express");
 
 const router = express.Router();
+// express.Router() is a function in Express.js that creates a new "mini" router or a modular, mountable route handler. Think of it like a mini version of the main Express app, designed to handle a specific group of routes.
 
 // importing the controller 
 const {createTodo} = require("../controllers/createTodo");
